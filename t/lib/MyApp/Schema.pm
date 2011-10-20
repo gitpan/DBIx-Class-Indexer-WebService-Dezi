17b6c8daa3acee08984897f1b50bd151d0b68f09
Logan Bell <logie@cpan.org>
2011-10-20 02:01:29 +0000

    Import of LOGIE/DBIx-Class-Indexer-WebService-Dezi-0.02 from CPAN.
    gitpan-cpan-distribution: DBIx-Class-Indexer-WebService-Dezi
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         LOGIE/DBIx-Class-Indexer-WebService-Dezi-0.02.tar.gz
    gitpan-cpan-author:       LOGIE
    gitpan-cpan-maturity:     released

--- t/lib/MyApp/Schema.pm
+++
@@ -1,8 +0,0 @@
-package MyApp::Schema;
-use base 'DBIx::Class::Schema';
-
-__PACKAGE__->load_classes(qw[
-    Person
-]);
-
-1;
--- t/sql/sqlite.sql
+++
@@ -1,9 +0,0 @@
-create table person (
-    person_id       integer primary key not null,
-    name            varchar(128) not null,
-    age             integer not null,
-    email           varchar(128),
-    image_path      varchar(128),
-    created         timestamp not null    
-);
-
